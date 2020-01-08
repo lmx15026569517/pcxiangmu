41742a70aae663e62c060aa8a3c0f00f9f6ed738
liys <18702198569@163.com>
2020-01-08 16:37:01 +0800

    准备创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import Login from '@/views/login'
 
 
 
 Vue.use(VueRouter) //  全局注册
 
-const routes = []
+//  配置路由表
+const routes = [{
+    path: '/login',
+    name: 'login',
+    component: () =>
+        import ('@/views/login')
+}]
 
 const router = new VueRouter({
     routes
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    登录组件
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LoginPage",
+    components: {},
+    props: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    methods: {}
+  };
+</script>
+
+<style scoped></style>
