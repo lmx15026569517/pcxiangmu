c41a9f089026531d0941cd02aa3699aec1e752d5
liys <18702198569@163.com>
2020-01-15 17:26:12 +0800

    编辑频道-获取所有频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -4,25 +4,28 @@
     <van-cell title="我的频道" :border="false">
         <van-button size="mini" round type="danger">编辑</van-button>
     </van-cell> 
+    <!-- bug  说我两个对象,就是:key没有写.id -->
       <van-grid :gutter="10">
       <van-grid-item
         v-for="channel in userChannels"
-        :key="channel"
+        :key="channel.id"
         :text="channel.name"
-      />
+      />  
+
 </van-grid>
     <van-cell title="推荐频道" :border="false"/>
       <van-grid :gutter="10">
       <van-grid-item
         v-for="value in 8"
         :key="value"
-        text="你可以"
+        text="文字"
       />
     </van-grid>
   </div>
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channel'
 export default {
   name: 'ChannelEdit',
   components: {},
@@ -35,13 +38,22 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      allChannels: [] //  所有频道
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadAllChannels()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadAllChannels () {
+      const { data } = await getAllChannels()
+      this.allChannels = data.data.channels
+    }
+  }
 }
 </script>
 
