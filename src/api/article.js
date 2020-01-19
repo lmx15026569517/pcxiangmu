cfb0401b40cfc286f2bd1d397094c984978e3cd7
liys <18702198569@163.com>
2020-01-19 23:04:47 +0800

    文章详情-请求展示

--- src/api/article.js
+++ src/api/article.js
@@ -20,3 +20,12 @@ export const getArticlesByChannel = params => {
     params
   })
 }
+/**
+ * 获取指定用户的文章列表
+ */
+export const getArticleById = articleId => {
+  return request({
+    method: 'GET',
+    url: `/app/v1_0/articles/${articleId}`
+  })
+}
